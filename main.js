2cd62191a4cdf5cf4e797c8554dd7d907e9f8755
yuyaK <yk1994k@gmail.com>
2023-01-12 23:08:24 +0900

    修正

--- main.js
+++ main.js
@@ -1,9 +1,55 @@
-const app = new Vue ({
+var STORAGE_KEY = 'todo-vuejs-demo'
+var todoStorage = {
+    fetch: function() {
+        var todos = JSON.parse(
+            localStorage.getItem(STORAGE_KEY) || '[]'
+        )
+        todos.forEach(function(todo, index) {
+            todo.id = index
+        })
+        todoStorage.uid = todos.length
+        return todos
+    },
+    save: function(todos) {
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+    }
+}
+
+new Vue ({
     el: '#app',
     data: {
-
+        todos: [
+            { id: 1, comment: 'タスク1', state: 0},
+            { id: 2, comment: 'タスク2', state: 0},
+            { id: 3, comment: 'タスク3', state: 0}
+        ]
     },
     methods: {
-        
+        doAdd: function(event, value) {
+            var comment = this.$refs.comment;
+
+            if (comment.value.length == 0) {
+                return;
+            }
+
+            this.todos.push({
+                id: todoStorage.uid++,
+                comment: comment.value,
+                state: 0
+            })
+
+            comment.value = '';
+        }
+    },
+    watch: {
+        todos: {
+            handler: function(todos) {
+                todoStorage.save(todos)
+            },
+            deep: true
+        }
+    },
+    created(){
+        this.todos = todoStorage.fetch();
     }
-})
+});
